36ec47cc79498bae886e8d05ea2ce365b66334ba
Peter <petercoker@protonmail.com>
2018-06-28 17:26:54 +0100

    first commit

---
+++ basic-concepts/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //One of the epic programs and the first of all programmers.Write a program in C# to print the magic words, Hello World. To do this you can use the method WriteLine. 
+            //Input
+            //    -
+            //    Output
+            //Hello World
+            //Solve
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ basic-concepts/reading-user-input/reading-user-input/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace reading_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program in C# that asks the user for their favorite color. The program should always respond in the following line: Mine too!
+            //    Input
+            //What is your favorite color? Red
+            //Output
+            //    Mine too!
+
+            Console.WriteLine("What is your favourite color? Red");
+            Console.ReadLine();
+            Console.WriteLine("Mine too!");
+
+        }
+    }
+}
