350b0ecfe7a5bb59ca7ccabbe8a5123884ecf9d4
Peter Coker <petercoker@protonmail.com>
2018-06-29 12:36:13 +0100

    complete exercise

---
+++ basic-concepts/basic-calculator/basic-calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace basic_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a basic calculador in C#  to perform a basic operations. The user can only enter a number on the screen. Ask the user for a number and then perform addition, subtraction, multiplication and division of the same number.
+            //    Input
+            //Enter a number: 4
+
+            int num1, num2;
+
+            Console.Write("Enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = num1++;
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0} * {1} = {2}",num1, num2, num1 * num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            
+            //Peter Coker
+        }
+    }
+}
---
+++ basic-concepts/multiplication-table/multiplication-table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to ask the user for a number and display its multiplication table, like this: 
+
+            //5 x 1 = 5
+            //5 x 2 = 10
+            //5 x 3 = 15
+            //    ...
+            //5 x 10 = 50
+
+            int number;
+
+
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            
+            for (int multiplcation = 1; multiplcation <= 10; multiplcation++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", number, multiplcation, number * multiplcation);
+            }
+
+            //Peter Coker
+
+        }
+    }
+}
