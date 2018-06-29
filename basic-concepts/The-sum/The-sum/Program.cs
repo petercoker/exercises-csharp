15e3387ef44cfdf25f35efb4524445568b167449
Peter Coker <petercoker@protonmail.com>
2018-06-29 10:03:21 +0100

    Complete the sum

---
+++ basic-concepts/The-sum/The-sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace The_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to print the result of adding 2 + 3 on screen. 
+            //Input
+            //    -
+            //    Output
+            //5
+
+            Console.WriteLine(2+3);
+
+        }
+    }
+}
