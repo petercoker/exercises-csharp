68bcba583abfc4aa5a7ee80bfd95a4049ca1d845
Peter Coker <petercoker@protonmail.com>
2018-06-29 15:12:18 +0100

    starting equivalent operations exercirse

---
+++ basic-concepts/equivalent-operations/equivalent-operations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace equivalent_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Peter Coker
+
+            //Write a C# program to ask the user for three numbers (a, b, c) and display the result of (a+b)·c and the result of a·c + b·c
+
+            //    Output
+
+
+
+        }
+    }
+}
