260afbbd829d0e7442cc78ecbc3bd65264dca96e
Peter Coker <petercoker@protonmail.com>
2018-06-29 16:05:02 +0100

    exerise

---
+++ basic-concepts/age/age/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to ask the user for his age (20, for instance) and answer something as "You look younger than 20" (instead of 20, you should display the age that has been entered).
+
+            //    Output
+        }
+    }
+}
---
+++ basic-concepts/formats/formats/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace formats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program to ask the user for a number and display ir four times in a row, separated with blank spaces, and then four times in the next row, with no separation. You must do it two times: first using Console.Write and then using {0}.
+
+            //    Output
+        }
+    }
+}
