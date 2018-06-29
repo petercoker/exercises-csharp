ee9b203189d8d3f236f4bd09a003ed077e9972ac
Peter Coker <petercoker@protonmail.com>
2018-06-29 15:52:30 +0100

    completed exercirse

--- basic-concepts/equivalent-operations/equivalent-operations/Program.cs
+++ basic-concepts/equivalent-operations/equivalent-operations/Program.cs
@@ -12,8 +12,31 @@ namespace equivalent_operations
 
             //    Output
 
+            //int number, sum = 0;
 
 
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    Console.Write("Enter a number {0}: ", i);
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            
+            //Console.WriteLine("Result by Addition and multiplication: {0}", sum); //Learn Arrays [i] could have been used
+
+            int number1, number2, number3;
+
+            Console.Write("Enter first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter third number: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Result of operation between ({0}, {1}, {2}) \n Result of operation between (a+b).c is {3} \n Result of operation between a.b + a.c is {4}", number1, number2, number3, ((number1 + number2) * number3), (number1 * number3 + number2* number3));
+            
         }
     }
 }
