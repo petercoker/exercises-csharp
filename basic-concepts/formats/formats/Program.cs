29040e4e154370c66f977f6c10ce887d87dba4d6
Peter Coker <petercoker@protonmail.com>
2018-06-30 20:22:23 +0100

    still working on separting the four lines

--- basic-concepts/formats/formats/Program.cs
+++ basic-concepts/formats/formats/Program.cs
@@ -9,6 +9,21 @@ namespace formats
             //Write a C# program to ask the user for a number and display ir four times in a row, separated with blank spaces, and then four times in the next row, with no separation. You must do it two times: first using Console.Write and then using {0}.
 
             //    Output
+
+            int number;
+
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.Write(number);
+            }
+
+
+
+
+
         }
     }
 }
