2b565f638025e6ef52234d779f4da8ad1524a5c0
Peter Coker <petercoker@protonmail.com>
2018-06-30 19:14:25 +0100

    completed age exercise

--- basic-concepts/age/age/Program.cs
+++ basic-concepts/age/age/Program.cs
@@ -9,6 +9,13 @@ namespace age
             //Write a C# program to ask the user for his age (20, for instance) and answer something as "You look younger than 20" (instead of 20, you should display the age that has been entered).
 
             //    Output
+
+            int age;
+
+            Console.Write("Please enter a your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look younger than {0} ", age);
+      
         }
     }
 }
