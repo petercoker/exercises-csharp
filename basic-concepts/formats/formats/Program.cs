88458d8d07f15c719e37e7d4344b70e9f215bca7
Peter Coker <petercoker@protonmail.com>
2018-07-01 22:12:13 +0100

    still working on

--- basic-concepts/formats/formats/Program.cs
+++ basic-concepts/formats/formats/Program.cs
@@ -10,15 +10,39 @@ namespace formats
 
             //    Output
 
-            int number;
+            //int number;
 
-            Console.Write("Enter a number: ");
-            number = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //number = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i <= 4; i++)
-            {
-                Console.Write(number);
-            }
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    Console.Write(number);
+            //}
+
+            int n;
+
+            Console.WriteLine("Enter a digit: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n);
+            Console.Write(" ");
+            Console.Write(n);
+            Console.Write(" ");
+            Console.Write(n);
+            Console.Write(" ");
+            Console.Write(n);
+            Console.WriteLine();
+
+            Console.Write(n);
+            Console.Write(n);
+            Console.Write(n);
+            Console.WriteLine(n);
+            Console.WriteLine();
+
+            Console.WriteLine("{0} {0} {0} {0}", n);
+
+            Console.WriteLine("{0}{0}{0}{0}", n);
 
 
 
