8972b96c933857765fe412be203e21af0c058b04
Peter Coker <petercoker@protonmail.com>
2018-07-03 14:48:49 +0100

    start conversion exercise

---
+++ basic-concepts/conversion/conversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a C# program to convert from celsius degrees to Kelvin and Fahrenheit: it will ask the user for the amount of celsius degrees and using the following conversion tables:
+
+            //    kelvin = celsius + 273
+
+            //fahrenheit = celsius x 18 / 10 + 32
+
+
+        }
+    }
+}
