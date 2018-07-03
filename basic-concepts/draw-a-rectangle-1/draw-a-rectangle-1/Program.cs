cd10d1abdc8e2f032694343d791fa3bb6d02b81e
Peter Coker <petercoker@protonmail.com>
2018-07-03 14:22:49 +0100

    started exercise

--- basic-concepts/draw-a-rectangle-1/draw-a-rectangle-1/Program.cs
+++ basic-concepts/draw-a-rectangle-1/draw-a-rectangle-1/Program.cs
@@ -6,6 +6,19 @@ namespace draw_a_rectangle_1
     {
         static void Main(string[] args)
         {
+            //Write a program in C # to draw a rectangle on the screen. The measurements of the rectangle will be 3 columns wide and 5 rows tall. Use a digit entered by the user to draw it.
+
+            //    Input
+            //7
+
+            //Output
+            //777
+            //777
+            //777
+            //777
+            //777
+
+
         }
     }
 }
