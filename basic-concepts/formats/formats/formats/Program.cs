b93f368a5ca7d85689d22a14a1ca363169671607
Peter Coker <petercoker@protonmail.com>
2018-07-03 14:07:25 +0100

    finished exercise

--- basic-concepts/formats/formats/formats/Program.cs
+++ basic-concepts/formats/formats/formats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace formats
 {
@@ -10,39 +11,60 @@ namespace formats
 
             //    Output
 
-            //int number;
+            int number;
 
-            //Console.Write("Enter a number: ");
-            //number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
 
-            //for (int i = 0; i <= 4; i++)
-            //{
-            //    Console.Write(number);
-            //}
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write(number  + " ");
+            }
 
-            int n;
+            Console.WriteLine(" ");
 
-            Console.WriteLine("Enter a digit: ");
-            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write(number);
+            }
 
-            Console.Write(n);
-            Console.Write(" ");
-            Console.Write(n);
-            Console.Write(" ");
-            Console.Write(n);
-            Console.Write(" ");
-            Console.Write(n);
-            Console.WriteLine();
+            Console.WriteLine(" ");
 
-            Console.Write(n);
-            Console.Write(n);
-            Console.Write(n);
-            Console.WriteLine(n);
-            Console.WriteLine();
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write("{0} {1}", number, " ");
+            }
 
-            Console.WriteLine("{0} {0} {0} {0}", n);
+            Console.WriteLine(" ");
 
-            Console.WriteLine("{0}{0}{0}{0}", n);
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write("{0}", number);
+            }
+
+            //int n;
+
+            //Console.Write("Enter a digit: ");
+            //n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write(n);
+            //Console.Write(" ");
+            //Console.Write(n);
+            //Console.Write(" ");
+            //Console.Write(n);
+            //Console.Write(" ");
+            //Console.Write(n);
+            //Console.WriteLine();
+
+            //Console.Write(n);
+            //Console.Write(n);
+            //Console.Write(n);
+            //Console.WriteLine(n);
+            //Console.WriteLine();
+
+            //Console.WriteLine("{0} {0} {0} {0}", n);
+
+            //Console.WriteLine("{0}{0}{0}{0}", n);
 
 
 
