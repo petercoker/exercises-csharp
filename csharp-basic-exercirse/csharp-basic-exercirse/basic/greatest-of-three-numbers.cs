9c065f4b5a61515ab1628314cc113c118598309d
Peter Coker <petercoker@protonmail.com>
2018-08-12 13:32:35 +0100

    completed exercise

--- csharp-basic-exercirse/csharp-basic-exercirse/basic/greatest-of-three-numbers.cs
+++ csharp-basic-exercirse/csharp-basic-exercirse/basic/greatest-of-three-numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using System.Runtime.Remoting.Services;
 
 namespace csharp_basic_exercirse.basic
@@ -10,19 +11,36 @@ namespace csharp_basic_exercirse.basic
             //Write a C# program to get three numbers from 
             //the user and display the greatest one.
 
-            string[] numbers = new string[3];
+            int number1, number2, number3;
 
-            for (int i = 1; i <= 3; i++)
+            Console.Write("Enter 1st number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 2nd number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter 3rd number: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > number2 && number1 > number3)
             {
-                Console.Write("Enter {0} number: ", i);
-                numbers [0] = Console.ReadLine();
-                numbers[1] = Console.ReadLine();
-                numbers[2] = Console.ReadLine();
+                Console.WriteLine("{0} is the great", number1);
             }
 
-            
-            
-            
+            else if ( number2 > number1 && number2 > number3)
+            {
+                Console.WriteLine("{0} is the great", number2);
+            }
+
+            else if (number3 > number1 && number3 > number2)
+            {
+                Console.WriteLine("{0} is the great", number3);
+            }
+
+            else
+            {
+                Console.WriteLine("Its equal!");
+            }
         }
         
     
